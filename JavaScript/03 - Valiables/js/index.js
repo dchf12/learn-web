6f7baa863c9ff89102173b6edf04dd0046099638
furuya <furuya003@gmail.com>
2022-04-20 17:31:17 +0900

    refactor: Compile ts to js for 03

---
+++ JavaScript/03 - Valiables/js/index.js
@@ -0,0 +1,7 @@
+var inputs = document.querySelectorAll('.controls input');
+function handleUpdate() {
+    var suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty("--".concat(this.name), this.value + suffix);
+}
+inputs.forEach(function (input) { return input.addEventListener('change', handleUpdate); });
+inputs.forEach(function (input) { return input.addEventListener('mousemove', handleUpdate); });
