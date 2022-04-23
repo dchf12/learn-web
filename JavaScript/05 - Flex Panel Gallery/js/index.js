18d43953556faf5e22c41620d6ea4a4f46aee93e
furuya <furuya003@gmail.com>
2022-04-23 14:47:27 +0900

    style: Formatting

--- JavaScript/05 - Flex Panel Gallery/js/index.js
+++ JavaScript/05 - Flex Panel Gallery/js/index.js
@@ -1,10 +1,14 @@
-"use strict";
+'use strict';
 const panels = document.querySelectorAll('.panel');
-panels.forEach((panel) => panel.addEventListener('click', () => {
+panels.forEach((panel) =>
+  panel.addEventListener('click', () => {
     panel.classList.toggle('open');
-}));
-panels.forEach((panel) => panel.addEventListener('transitionend', ((e) => {
+  })
+);
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', (e) => {
     if (e.propertyName.includes('flex')) {
-        panel.classList.toggle('open-active');
+      panel.classList.toggle('open-active');
     }
-})));
+  })
+);
--- JavaScript/06 - Type Ahead/js/index.js
+++ JavaScript/06 - Type Ahead/js/index.js
@@ -1,52 +1,53 @@
-"use strict";
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+'use strict';
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 // 非同期にリソース取得
 fetch(endpoint)
-    .then((response) => {
+  .then((response) => {
     if (!response.ok) {
-        throw new Error(response.statusText);
+      throw new Error(response.statusText);
     }
     return response.json();
-})
-    .then((data) => {
+  })
+  .then((data) => {
     cities.push(...data);
-})
-    .catch((e) => {
+  })
+  .catch((e) => {
     console.error('fetch error', e);
-});
+  });
 function findMatches(wordToMatch, cities) {
-    return cities.filter((place) => {
-        // マッチするものだけを返す
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
-    });
+  return cities.filter((place) => {
+    // マッチするものだけを返す
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
 }
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray
-        .map((place) => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+      return `
       <li>
         <span class="name">${cityName}, ${stateName}</span>
         <span class="population">${numberWithCommas(place.population)}</span>
       </li>
     `;
     })
-        .join('');
-    assertIsDefined(suggestions);
-    suggestions.innerHTML = html;
+    .join('');
+  assertIsDefined(suggestions);
+  suggestions.innerHTML = html;
 }
 function assertIsDefined(val) {
-    if (val === undefined || val === null) {
-        throw new Error(`Expected 'val' to be defined, but received ${val}`);
-    }
+  if (val === undefined || val === null) {
+    throw new Error(`Expected 'val' to be defined, but received ${val}`);
+  }
 }
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
